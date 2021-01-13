c322d321867a65b8696483bd3bf2e7c1e71de89f
Tommy Shi <tommyshi19991130@gmail.com>
2021-01-13 20:20:42 +1300

    Update searchRestaurant to search based on location

--- firebase_functions/functions/index.js
+++ firebase_functions/functions/index.js
@@ -6,16 +6,27 @@ const baseUrl = "https://developers.zomato.com/api/v2.1";
 
 exports.searchRestaurants = functions.https.onRequest((req, res) => {
   // search restaurant via zomato api
+  const lat = req.query.lat;
+  const lon = req.query.lon;
   axios.get(`${baseUrl}/search`, {
-    headers: {'user-key': process.env.ZOMATO_API_KEY}
+    headers: {'user-key': process.env.ZOMATO_API_KEY},
+    params: {
+      lat: lat,
+      lon: lon,
+      sort: 'real_distance'
+    }
   })
   .then((response) => {
+    // TODO: Format the data to be more meaningful 
     res.status(200).json({
-      message: response.data
+      success: true,
+      response: response.data
     });
   })
   .catch((err) => {
+    console.log(err);
     res.status(500).json({
+      success: false,
       message: 'Error fetching Zomato API'
     });
   });
