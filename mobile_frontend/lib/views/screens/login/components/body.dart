d66405afa9e9f851fdbff5b3b868e8e846246371
Tommy Shi <tommyshi19991130@gmail.com>
2021-01-23 11:40:57 +1300

    Implement login with email functionalities

--- mobile_frontend/lib/views/screens/login/components/body.dart
+++ mobile_frontend/lib/views/screens/login/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mobile_frontend/views/components/already_have_an_account_check.dart';
@@ -7,44 +8,77 @@ import 'package:mobile_frontend/views/components/rounded_input_field.dart';
 import 'package:mobile_frontend/views/components/rounded_password_field.dart';
 import 'package:mobile_frontend/views/screens/main/main_screen.dart';
 import 'package:mobile_frontend/views/screens/signup/sign_up_screen.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'LOGIN',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SvgPicture.asset(
-            'assets/icons/login.svg',
-            height: size.height * 0.35,
-          ),
-          RoundedInputField(
-            hintText: 'Your Email',
-            onChanged: (value) {},
-          ),
-          RoundedPasswordField(
-            onChanged: (value) {},
-          ),
-          RoundedButton(
-            text: 'LOGIN',
-            onPressed: () {
-              // TODO Implement authentication check
-              Navigator.pushNamed(context, MainScreen.id);
-            },
-          ),
-          SizedBox(height: size.height * 0.03),
-          AlreadyHaveAnAccountCheck(
-            onPressed: () {
-              Navigator.pushNamed(context, SignUpScreen.id);
-            },
-          ),
-        ],
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Background(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'LOGIN',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SvgPicture.asset(
+              'assets/icons/login.svg',
+              height: size.height * 0.35,
+            ),
+            RoundedInputField(
+              hintText: 'Your Email',
+              onChanged: (value) {
+                email = value;
+              },
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {
+                password = value;
+              },
+            ),
+            RoundedButton(
+              text: 'LOGIN',
+              onPressed: () async {
+                setState(() {
+                  showSpinner = true;
+                });
+                try {
+                  final user = await _auth.signInWithEmailAndPassword(
+                      email: email, password: password);
+                  if (user != null) {
+                    print(user);
+                    Navigator.pushNamed(context, MainScreen.id);
+                  }
+                  setState(() {
+                    showSpinner = false;
+                  });
+                } catch (e) {
+                  // TODO: add error handling
+                  print(e);
+                }
+              },
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              onPressed: () {
+                Navigator.pushNamed(context, SignUpScreen.id);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
