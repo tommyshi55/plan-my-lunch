d8644dbca07522c56b1a662bb5dc1f52723621bc
Tommy Shi <tommyshi19991130@gmail.com>
2021-01-23 14:59:58 +1300

    Enable user sign out

--- mobile_frontend/lib/views/screens/main/main_screen.dart
+++ mobile_frontend/lib/views/screens/main/main_screen.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_frontend/views/constants.dart';
 import 'package:mobile_frontend/views/screens/main/components/body.dart';
 
+import '../welcome/welcome_screen.dart';
+
 class MainScreen extends StatelessWidget {
   static const String id = 'main_screen';
+  final _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +18,13 @@ class MainScreen extends StatelessWidget {
         backgroundColor: Colors.transparent,
         title: Text('PLAN MY LUNCH'),
         automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(Icons.logout),
+          onPressed: () {
+            _auth.signOut();
+            Navigator.popUntil(context, ModalRoute.withName(WelcomeScreen.id));
+          },
+        ),
       ),
       body: Body(),
     );
