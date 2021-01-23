14c15b546d54f939800e921f98125c221b07a762
Tommy Shi <tommyshi19991130@gmail.com>
2021-01-23 23:38:40 +1300

    remove commented out code

--- mobile_frontend/lib/business_logic/services/restaurant_service.dart
+++ mobile_frontend/lib/business_logic/services/restaurant_service.dart
@@ -33,22 +33,5 @@ class RestaurantService {
     } else {
       throw Exception('Error retrieving restaurants');
     }
-
-    // int randNum = Random().nextInt(10) + 1;
-    //
-    // for (int i = 0; i < randNum; i++) {
-    //   restaurants.add(Restaurant(
-    //       "7005126",
-    //       "Welcome Eatery",
-    //       "Orion Building, 181 Grafton Road, Grafton, Auckland 1010",
-    //       "Grafton",
-    //       "3.9",
-    //       "10",
-    //       "https://b.zmtcdn.com/data/pictures/6/7005126/8705514d0e6f26043fea380a3bd1ef18.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A",
-    //       "https://www.zomato.com/auckland/welcome-eatery-grafton?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1"));
-    // }
-    //
-    // sleep(Duration(seconds: 1));
-    return restaurants;
   }
 }
