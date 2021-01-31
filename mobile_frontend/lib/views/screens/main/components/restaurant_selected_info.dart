43932ff198eec555c4d7464c14e400c6085f4b32
Tommy Shi <tommyshi19991130@gmail.com>
2021-01-31 22:51:51 +1300

    Add loading indicator when cancelling restaurant selection

--- mobile_frontend/lib/views/screens/main/components/restaurant_selected_info.dart
+++ mobile_frontend/lib/views/screens/main/components/restaurant_selected_info.dart
@@ -7,9 +7,10 @@ import 'package:mobile_frontend/business_logic/models/user_plan.dart';
 import 'package:mobile_frontend/views/components/rounded_button.dart';
 import 'package:mobile_frontend/views/constants.dart';
 import 'package:mobile_frontend/views/screens/search_restaurants/components/restaurant_card.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:provider/provider.dart';
 
-class RestaurantSelectedInfo extends StatelessWidget {
+class RestaurantSelectedInfo extends StatefulWidget {
   final Restaurant restaurant;
   final DateTime date;
   final User user;
@@ -18,38 +19,57 @@ class RestaurantSelectedInfo extends StatelessWidget {
       : super(key: key);
 
   @override
+  _RestaurantSelectedInfoState createState() => _RestaurantSelectedInfoState();
+}
+
+class _RestaurantSelectedInfoState extends State<RestaurantSelectedInfo> {
+  bool loading = false;
+
+  @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Hooray! On this day, you are going to:',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: kPrimaryColor,
-            fontWeight: FontWeight.bold,
-            fontSize: 20.0,
+    return ModalProgressHUD(
+      inAsyncCall: loading,
+      opacity: 0,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Hooray! On this day, you are going to:',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: kPrimaryColor,
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+            ),
           ),
-        ),
-        SizedBox(
-          height: size.height * 0.05,
-        ),
-        RestaurantCard(
-          restaurant: restaurant,
-          isInSearchResult: false,
-        ),
-        SizedBox(
-          height: size.height * 0.05,
-        ),
-        RoundedButton(
-          text: 'CANCEL AND EDIT THE PLAN',
-          onPressed: () async {
-            await Provider.of<UserPlan>(context, listen: false)
-                .updatePlan(date, user, SelectedPlan(planType: Selection.none));
-          },
-        ),
-      ],
+          SizedBox(
+            height: size.height * 0.05,
+          ),
+          RestaurantCard(
+            restaurant: widget.restaurant,
+            isInSearchResult: false,
+          ),
+          SizedBox(
+            height: size.height * 0.05,
+          ),
+          RoundedButton(
+            text: 'CANCEL AND EDIT THE PLAN',
+            onPressed: () async {
+              setState(() {
+                loading = true;
+              });
+              await Provider.of<UserPlan>(context, listen: false).updatePlan(
+                  widget.date,
+                  widget.user,
+                  SelectedPlan(planType: Selection.none));
+              setState(() {
+                loading = false;
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
